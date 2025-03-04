8b50e7c2c47ad21a2c6bfbce4e8ed2a4a2f13b34
chike <chikeegonu@gmail.com>
2025-03-04 17:00:01 +0100

    daily_commit: 2025-03-04 17:00:01

--- java/course_exercise/Controlling-Program-Flow-in-Java/02/demos/exercise-files/before/Main.java
+++ java/course_exercise/Controlling-Program-Flow-in-Java/02/demos/exercise-files/before/Main.java
@@ -1,22 +1,20 @@
 
 class Main {
     public static void main(String[] args) {
-        int value1 = 10, value2 = 4, diff = 0;
-        
-        if (value1 > value2) 
-            diff = value1 - value2;
-        
+        int output = 9;
 
-
-        else 
-            diff = value2 - value1;
-            System.out.println("Value 1 is not bigger than value 2, diff= " + diff);
-
-        // if (value1 > value2) {
-        //     diff = value1 - value2;
-        // } else {
-        //     diff = value2 - value1;
-        //     System.out.println("Value 1 is not bigger than value 2, diff= " + diff);
-        // }
+        switch (output){
+            case 0:
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("default");
+        }
     }
 }
